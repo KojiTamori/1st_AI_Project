05ddbf50947683e982ecff4fcfaca08fec88cabe
unknown <k.tamori>
2019-05-12 16:22:26 +0900

    1st commit

--- Django_folda/predict/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'predict'
-urlpatterns = [
-    path('top_page/', views.top_page, name='top_page'),
-    path('predict/', views.predict, name='predict'),
-]
